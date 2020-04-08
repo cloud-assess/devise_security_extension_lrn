654a0018da7d728890dc0dace11b2f3206241478
Vitalya Shevtsov <vitaliy@cloudassess.com.au>
2020-04-09 08:31:30 +1000

    update url for ca-leaner

--- lib/devise-security/controllers/helpers.rb
+++ lib/devise-security/controllers/helpers.rb
@@ -79,7 +79,8 @@ module DeviseSecurity
 
         # redirect for password update with alert message
         def redirect_for_password_change(scope)
-          redirect_to change_password_required_path_for(scope), alert: I18n.t('change_required', {scope: 'devise.password_expired'})
+          url = "#{ENV['MAIN_APP_URL'].gsub('<replace>', request.subdomain.delete('-lrn'))}/#{scope}/password_expired"
+          redirect_to url, alert: I18n.t('change_required', {scope: 'devise.password_expired'})
         end
 
         def redirect_for_paranoid_verification(scope)
